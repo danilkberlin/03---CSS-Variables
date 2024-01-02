89b59d07fbb96430cf7493d9d4a85cd017e9bcb2
danilkberlin <danilkravets19@gmail.com>
2024-01-02 15:04:09 +0100

    мы создали переменную в которой сказали что либо показывается sizing либо же ничего

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const inputs = document.querySelectorAll('.controls input')
 function handlUpdate() {
-    console.log(this.value)
+    const suffix = this.dataset.sizing || '';
+    console.log(suffix)
 }
 inputs.forEach( input => input.addEventListener('change', handlUpdate,))
 inputs.forEach( input => input.addEventListener('mousemove', handlUpdate))
