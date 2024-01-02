4af3ec58fad416ca9313143bab25a635316cd389
danilkberlin <danilkravets19@gmail.com>
2024-01-02 14:50:37 +0100

    Сделали loop

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const inputs = document.querySelectorAll('.controls input')
 function handlUpdate() {
-    
+    console.log(this.value)
 }
+inputs.forEach( input => input.addEventListener('change', handlUpdate,)
+)
