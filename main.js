763962a62e910606165bd0005d9b82d47afc9ad0
danilkberlin <danilkravets19@gmail.com>
2024-01-02 14:55:28 +0100

    Мы создали так же ещё один loop в котором указали что теперь при постоянном изменении будет показывать

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const inputs = document.querySelectorAll('.controls input')
 function handlUpdate() {
     console.log(this.value)
 }
-inputs.forEach( input => input.addEventListener('change', handlUpdate,)
-)
+inputs.forEach( input => input.addEventListener('change', handlUpdate,))
+inputs.forEach( input => input.addEventListener('mousemove', handlUpdate))
