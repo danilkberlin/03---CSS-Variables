cd3a1add8ec3aaa62a7509ea81bd8abdd820586e
danilkberlin <danilkravets19@gmail.com>
2024-01-02 15:10:35 +0100

    В function мы побавили так же управление стилем по имени и варианту

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const inputs = document.querySelectorAll('.controls input')
 function handlUpdate() {
     const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
     console.log(suffix)
 }
 inputs.forEach( input => input.addEventListener('change', handlUpdate,))
